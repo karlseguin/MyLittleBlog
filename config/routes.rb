a496cd0581d98de6636e31f428c2cbbc7af9b94c
Karl Seguin <karlseguin@gmail.com>
2010-11-30 23:32:01 +0800

    minimalist

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 OpenMyMind::Application.routes.draw do
   
-  match '/adnim/index/:page' => 'adnin#index'
-#  match '/adnim/:action/:id' => 'adnin' :controller => 'adnim'
+  match '/adnim/index/:page' => 'adnim#index'
+  match '/adnim/(:action)(/:id)' => 'adnim'
   
   match '/rss' => 'home#rss'
   match ':year/:month/:day/:slug' => 'post#view'
-  match'/:category/:page' => 'home#index'
+  match '/:category/(:page)' => 'home#index'
   
   root :to => 'home#index'
 end
