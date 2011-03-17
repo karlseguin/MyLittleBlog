105ae2d3a4f78ac8ffef0742b902112fc93237bc
Karl Seguin <karlseguin@gmail.com>
2011-03-17 19:43:37 +0800

    dropped time from date

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 OpenMyMind::Application.initialize!
 
-Time::DATE_FORMATS[:default] = '%A %B %d, %H:%d'
+Time::DATE_FORMATS[:default] = '%A %B %d'
